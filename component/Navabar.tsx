54abd7ec899603c92f159c3bae61f7a7a30169fa
haxuanhuy1 <huyha866@gmail.com>
2023-11-13 15:48:49 +0700

    demo5

--- component/Navabar.tsx
+++ component/Navabar.tsx
@@ -7,11 +7,7 @@ export default function Navabar() {
  
   return (
    <div>
-    <p>Aloo anh em</p>
-    <p>branch</p>
-    <h1>Navarbar</h1>
-    <p>Hello nextjs</p>
-    <p>HEllo anh em</p>
+   <p>hà xuân huy</p>
    </div>
   );
 }
